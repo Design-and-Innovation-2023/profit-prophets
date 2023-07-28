fd72f50ecebde4fc722e5d8b8bd68daf177a2a7f
Kai En Yong <kaien.yong99@gmail.com>
2023-07-28 01:18:11 +0100

    Added vertical timeline component (WIP)

--- team7b-app/src/HomePage.tsx
+++ team7b-app/src/HomePage.tsx
@@ -9,13 +9,12 @@ const HomePage = () => {
             Welcome to Team 7B's Diary
         </Typography>
         <Typography variant="h6" component="h2" sx={{ fontWeight: 'medium', color: 'text.secondary', mt: 2, mb: 2, lineHeight: 1.5 }}>
-            Our tool aims to solve the difficulty of comparing various savings accounts between different banks. We provide an in-depth comparison with ease of use at the forefront of our design.
+            Our project aims to design a tool to solve the difficulty of comparing various savings accounts between different banks. We provide an in-depth comparison with ease of use at the forefront of our design.
         </Typography>
         <Typography variant="body1" component="p" sx={{ fontWeight: 'light', color: 'text.secondary', mt: 2, mb: 2, lineHeight: 1.6 }}>
             Navigate to different sections to learn more about our journey, reflections, and proud moments.
         </Typography>
         </Container>
-
     </Box>
   );
 };
--- team7b-app/src/ProjectJourneyPage.tsx
+++ team7b-app/src/ProjectJourneyPage.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 const ProjectJourneyPage = () => {
-    return (
-        <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Project Journey Page</h1>
-        </div>
-    );
+  return (
+    <div style={{backgroundColor: '#ededed', minHeight: '100vh'}}>
+      <h1>Project Journey</h1>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2023-01-01"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">Conceptualization</h3>
+          <p>Initial brainstorming and discussions about various ideas.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2023-02-01"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">Choosing the Current Idea</h3>
+          <p>Explanation of why we decided to go with our current concept.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2023-03-01"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">Development Iteration</h3>
+          <p>Description of different versions, improvements, and setbacks.</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2023-04-01"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">Overcoming Setbacks</h3>
+          <p>Explanation of how we overcame various obstacles.</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 };
 
 export default ProjectJourneyPage;
